b63818c63960dbf6b11e7827b11eb637d184fd51
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-11-24 17:02:33 -0300

    feat: adding differences for int and string

--- scripts/conditionals.sh
+++ scripts/conditionals.sh
@@ -3,12 +3,22 @@
 var1="b"
 var2="a"
 
+declare -i n1=1
+declare -i n2=2
+
 #---------------------------------------#
 # Normal conditional
 if [[ "$var1" = "$var2" ]]; then
     echo "same"
 else
     echo "different"
+fi
+
+# There are test operators specific for text and numbers
+if [[ $n1 -gt $n2 ]]; then
+    echo "$n1 > $n2"
+else
+    echo "$n1 < $n2"
 fi
 
 if [[ "$var1" = "$var2" ]]
