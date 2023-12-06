8b5b966a3907303eb8fcae03459bb6ea67b6acc7
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-12-06 17:39:49 -0300

    feat: creating shell script for run c project

---
+++ execute_c_project.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+libs=(
+    lib1.c
+    lib2.c
+)
+
+for i in ${libs[@]}; do
+    gcc i -c
+done
+
+# TODO
+# Generalize the cases for -o
+# Maybe using the --print-file-log from gcc and editing the last character from .c to .o
+
+gcc lib1.c -c
+gcc lib2.c -c
+gcc main.c -c
+gcc main.o lib1.o lib2.o -o main
+
+clear
+./main
+
+rm main.o lib1.o lib2.o main
