67cd3a0d572cf917fcc1313a037a1846ff956e54
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-12-11 15:36:59 -0300

    feat: creating script to add fonts in fedora linux

---
+++ fedora_font_add.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+declare -i d=0
+declare -i f=0
+
+for i in $@; do
+    if [ $i == '-d' ]; then
+        d=1
+    fi
+
+    if [ $i == '-f' ]; then
+        f=1
+    fi
+done
+
+if [ $d -eq 0 || $f -eq 0 ]; then
+    echo "Choose a directory name with -d and a file with -f"
+    exit 1
+fi
+
+declare -i count=0
+while [ $count -lt $# ]; do
+    if [ ${!count} == "-d" ]; then
+        declare -i dir=$((count+1))
+        sudo mkdir -p /usr/local/share/fonts/${!dir}
+    fi
+
+    if [ ${!count} == "-f" ]; then
+        declare -i file=$((count+1))
+        sudo cp ${!file} /usr/local/share/fonts/${!dir}
+        sudo chown -R root: /usr/local/share/fonts/${!dir}
+        sudo chmod 644 /usr/local/share/fonts/${!dir}/*
+        sudo restorecon -vFr /usr/local/share/fonts/${!dir}
+        sudo fc-cache -v
+    fi
+done
